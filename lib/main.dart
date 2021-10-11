27614c96d8487a1ef85c18bd382924845c1ffd92
sharifc2016 <sharifc2016@gmail.com>
2021-10-11 22:28:00 +0600

    add screen routes and create screen pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,29 @@
+import 'package:bit_chat/constants.dart';
+import 'package:bit_chat/screens/chat_screen.dart';
+import 'package:bit_chat/screens/login_screen.dart';
+import 'package:bit_chat/screens/registration_screen.dart';
+import 'package:bit_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp());
+void main() => runApp(BitChat());
+
+class BitChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          textTheme: TextTheme(
+            bodyText2: TextStyle(
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        initialRoute: kScreens.welcome_screen,
+        routes: {
+          kScreens.welcome_screen: (context) => WelcomeScreen(),
+          kScreens.login_screen: (context) => LoginScreen(),
+          kScreens.registration_screen: (context) => RegistrationScreen(),
+          kScreens.chat_screen: (context) => ChatScreen(),
+        });
+  }
 }
